868a0d198d093780cb4471a240b2c250ad28e519
jeremiahUdom <116927290+jeremiahUdom@users.noreply.github.com>
2025-07-24 20:01:18 +0100

    Update app.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,20 +1,84 @@
 const targetCurrency = document.querySelector("#target_currency");
+const targetAmt = document.querySelector("#target_amt");
+const amountToConvert = document.querySelector("#amt");
+const mainDiv = document.querySelector("main");
+const baseCurrency = document.querySelector("#base_currency");
 
-targetCurrency.addEventListener("change", async (e) => {
-  const baseCurrency = document.querySelector("#base_currency").value;
-  const amountToConvert = document.querySelector("#amt").value;
+const displayConversionRate = (baseCurrency, targetCurrency, conversionRate) => {
+  let conversionRateText = document.querySelector(".conversion_rate_text");
+  const containsConversionRate = mainDiv.contains(conversionRateText);
+
+  if (containsConversionRate) {
+    conversionRateText.textContent = `1 ${baseCurrency} = ${conversionRate} ${targetCurrency}`;
+    return;
+  }
+
+  conversionRateText = document.createElement("p");
+  conversionRateText.className = "conversion_rate_text";
+  conversionRateText.textContent = `1 ${baseCurrency} = ${conversionRate} ${targetCurrency}`;
+  mainDiv.appendChild(conversionRateText);
+}
+
+const convertCurrency = async (baseCurrency, targetCurrency, amountToConvert) => {
+  const response = await axios.get("/convert", {
+    params: {
+      from: baseCurrency,
+      to: targetCurrency,
+      amount: amountToConvert,
+    }
+  });
+
+  const data = {
+    conversionRate: response.data.conversionRate,
+    conversionResult: response.data.conversionResult
+  }
+  
+  return data;
+}
+
+amountToConvert.addEventListener("input", async (e) => {
+  if (baseCurrency.value === "select a currency" || targetCurrency.value === "select a currency"){
+    return;
+  }
 
   try {
-    const response = await axios.get("/convert", {
-      params: {
-        from: baseCurrency,
-        to: e.target.value,
-        amount: amountToConvert,
-      }
-    });
+    targetAmt.value = "Hang tight.....";
+    const result = await convertCurrency(baseCurrency.value, targetCurrency.value, e.target.value);
+    const { conversionRate, conversionResult } = result;
+    displayConversionRate(baseCurrency.value, targetCurrency.value, conversionRate);
+    targetAmt.value = conversionResult;
+  } catch (error) {
+    console.error("error")
+  }
+});
 
-    const { result } = response.data;
-    targetCurrency.value = result;
+baseCurrency.addEventListener("change", async (e) => {
+  if (targetCurrency.value === "select a currency" || amountToConvert.value.trim() === "") {
+    return;
+  }
+
+  try {
+    targetAmt.value = "Hang tight.....";
+    const result = await convertCurrency(e.target.value, targetCurrency.value, amountToConvert.value);
+    const { conversionRate, conversionResult } = result;
+    displayConversionRate(e.target.value, targetCurrency.value, conversionRate);
+    targetAmt.value = conversionResult;
+  } catch (error) {
+    console.error("error")
+  }
+});
+
+targetCurrency.addEventListener("change", async (e) => {
+  if (baseCurrency.value === "select a currency" || amountToConvert.value.trim() === "") {
+    return;
+  }
+
+  try {
+    targetAmt.value = "Hang tight.....";
+    const result = await convertCurrency(baseCurrency.value, e.target.value, amountToConvert.value);
+    const { conversionRate, conversionResult } = result;
+    displayConversionRate(baseCurrency.value, e.target.value, conversionRate);
+    targetAmt.value = conversionResult;
   } catch (error) {
     console.error("error")
   }
--- server.js
+++ server.js
@@ -16,17 +16,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const getAllSuporttedCurrencies = async () => {
   let supportedCurrencies = [];
   try {
-    const result = await axios.get(`${BASE_API_URL}/list`, {
-      params: {
-        access_key: API_KEY
+    const result = await axios.get(`${BASE_API_URL}/${API_KEY}/codes`);
+    supportedCurrencies = result.data.supported_codes.map(row => {
+      return {
+        label: `${row[0]} - ${row[1]}`,
+        code: row[0]
       }
     });
-    supportedCurrencies = Object.entries(result.data.currencies).map(([currencyCode, value]) => (
-      {
-        label: `${currencyCode} - ${value}`,
-        code: currencyCode
-      }
-    ))
   } catch (error) {
     console.error(error);
   }
@@ -36,21 +32,16 @@ const getAllSuporttedCurrencies = async () => {
 
 const convertFromOneCurrencyToAnother = async (baseCurrency, targetCurrency, amt) => {
   let conversionResult = 0;
+  let conversionRate = 0;
   try {
-    const result = await axios.get(`${BASE_API_URL}/convert`, {
-      params: {
-        access_key: API_KEY,
-        from: baseCurrency,
-        to: targetCurrency,
-        amount: amt
-      }
-    });
-    conversionResult = result.data.result;
+    const result = await axios.get(`${BASE_API_URL}/${API_KEY}/pair/${baseCurrency}/${targetCurrency}/${amt}`);
+    conversionResult = result.data.conversion_result;
+    conversionRate = result.data.conversion_rate;
   } catch (error) {
     console.error(error);
   }
 
-  return conversionResult;
+  return { conversionResult, conversionRate };
 }
 
 app.get("/", async (req, res) => {
@@ -61,7 +52,10 @@ app.get("/", async (req, res) => {
 app.get("/convert", async (req, res) => {
   const { from, to, amount } = req.query;  
   const result = await convertFromOneCurrencyToAnother(from, to, amount);
-  res.json({result}); //send result back to the frontend.
+  res.json({
+    conversionRate: result.conversionRate,
+    conversionResult: result.conversionResult
+  }); //send result back to the frontend.
 });
 
 app.listen(port, () => {
